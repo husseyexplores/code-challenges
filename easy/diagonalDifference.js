70cf90ea86b37ed027c65cabff0af02572aa0175
husseyexplores <husseyexplores@gmail.com>
2018-09-12 10:04:04 +0500

    Complete diagonalDifference challenge

--- easy/diagonalDifference.js
+++ easy/diagonalDifference.js
@@ -1,5 +1,30 @@
-const diagonalDifference = arr = {
+const diagonalDifference = arr => {
 
+  /*
+    [
+      [1,2,3] , - 0
+      [4,5,6] , - 1
+      [9,8,9] , - 2
+    ]
+
+    1 => [0][0] 
+    5 => [1][1]
+    9 => [2][2]
+    --
+    3 => [0][arr.length - 1]
+    5 => [1][arr.length - (1 +1)]
+    9 => [2][arr.length - (1 +2)]
+  */
+
+  let ltr = 0;
+  let rtl = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    ltr += arr[i][i];
+    rtl += arr[i][arr.length - (i + 1)];
+  }
+
+  return Math.abs(ltr - rtl);
 };
 
 module.exports = diagonalDifference
