eae54b480a3d87db8acc9933734f7b789532ead8
husseyexplores <husseyexplores@gmail.com>
2018-09-14 17:29:48 +0500

    Add caesar cipher solution

--- easy/caesarCipher.js
+++ easy/caesarCipher.js
@@ -1,5 +1,19 @@
 const caesar = (string, shift) => {
-
+  return string.split('').map(char => charShift(char, shift)).join('');
 };
+
+const codeSet = code => code > 97 ? 97 : 65;
+
+const mod = (n, m) => ((n % m) + m) % m;
+
+const charShift = (char, shift) => {
+  let code = char.charCodeAt();
+
+  if (char.match(/[A-Za-z]/g)) {
+    code = mod((code + shift) - codeSet(code), 26) + codeSet(code);
+  }
+
+  return String.fromCharCode(code)
+}
 
 module.exports = caesar;
