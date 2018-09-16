94ccdd4f8d9b0ce5c0ec8ea4b58cd1ba1e9bd40e
husseyexplores <husseyexplores@gmail.com>
2018-09-16 15:33:02 +0500

    Fix typo

--- easy/leapYear.test.js
+++ easy/leapYear.test.js
@@ -1,37 +1,37 @@
-const learYear = require('./leapYear');
+const leapYear = require('./leapYear');
 
 describe('Check Leap Years', () => {
   it('year not divisible by 4: common year', () => {
-    expect(learYear(2015)).toBeFalsy();
+    expect(leapYear(2015)).toBeFalsy();
   });
 
   it('year divisible by 4, not divisible by 100: leap year', () => {
-    expect(learYear(2016)).toBeTruthy();
+    expect(leapYear(2016)).toBeTruthy();
   });
 
   it('year divisible by 100, not divisible by 400: common year', () => {
-    expect(learYear(2100)).toBeFalsy();
+    expect(leapYear(2100)).toBeFalsy();
   });
 
   it('year divisible by 400: leap year', () => {
-    expect(learYear(2000)).toBeTruthy();
+    expect(leapYear(2000)).toBeTruthy();
   });
   it('works with non century years', function () {
-    expect(learYear(1996)).toBeTruthy();
+    expect(leapYear(1996)).toBeTruthy();
   });
   it('works with non century years', function () {
-    expect(learYear(1997)).toBeFalsy();
+    expect(leapYear(1997)).toBeFalsy();
   });
   it('works with ridiculously futuristic non century years', function () {
-    expect(learYear(34992)).toBeTruthy();
+    expect(leapYear(34992)).toBeTruthy();
   });
   it('works with century years', function () {
-    expect(learYear(1900)).toBeFalsy()
+    expect(leapYear(1900)).toBeFalsy()
   });
   it('works with century years', function () {
-    expect(learYear(1600)).toBeTruthy();
+    expect(leapYear(1600)).toBeTruthy();
   });
   it('works with century years', function () {
-    expect(learYear(700)).toBeFalsy()
+    expect(leapYear(700)).toBeFalsy()
   });
 });
